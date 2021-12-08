e095b5b6f4e477269b8255b244ee779ed4e733e4
cephaschapa <cephaschapa@gmail.com>
2021-12-08 18:57:02 +0200

    update menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
+import {Route} from 'react-router-dom'
 import HomePage from './page/homepage/homepage.component'
+
+const HatsPage = () =>{
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  )
+}
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+        <Route path='/' component={HomePage}/>
+        <Route path='/hats' component={HatsPage}/>
     </div>
   );
 }
 
+
+
 export default App;
+
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
 import './menu-items.styles.scss'
-const MenuItem = ({title, imageUrl,size }) =>{
-    return(
-        <div className={`${size} menu-item `}>
-            <div className="background-image" style={{backgroundImage:`url(${imageUrl})`, backgroundSize:'cover', backgroundRepeat: 'no-repeat', backgroundPosition:'center'}}>
 
-            </div>
-            <div className="content">
-                <h1 className="title">{title.toUpperCase()}</h1>
-                <span className="subtite">SHOP NOW</span>
-            </div>
-        </div>
+const MenuItem = ({title, imageUrl,size, history, match, linkUrl }) =>{
+    return(
+           <div className={`${size} menu-item `} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
+                <div className="background-image" style={{backgroundImage:`url(${imageUrl})`, backgroundSize:'cover', backgroundRepeat: 'no-repeat', backgroundPosition:'center'}}>
+
+                </div>
+                <div className="content">
+                    <h1 className="title">{title.toUpperCase()}</h1>
+                    <span className="subtite">SHOP NOW</span>
+                </div>
+            </div>    
     )
 }
 
-export default MenuItem;
+
+export default withRouter(MenuItem)
+
