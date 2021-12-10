092957eda8b61f199a378584d0a86c4be1831878
cephaschapa <cephaschapa@gmail.com>
2021-12-10 11:50:17 +0200

    added header comonents and links to home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
-import {Route} from 'react-router-dom'
+import {Route,Switch} from 'react-router-dom'
 import HomePage from './page/homepage/homepage.component';
 import ShopPage from './page/shop/shoppage.component';
+import Header from './components/header-component/header.component'
 
 function App() {
   return (
     <div>
+      <Header />
+      <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
+      </Switch>
     </div>
   );
 }
